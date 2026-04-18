2131249542b5031ee51c421c10e382dfcd4a3d7c
Stepan Tyurin <sanstepon5@gmail.com>
2026-04-18 18:19:15 +0200

    Changes to building menu ui

--- scripts/building.gd
+++ scripts/building.gd
@@ -7,7 +7,14 @@ var title: String = "~  Placeholder Building cool  ~"
 @export var selected_upgrade: Upgrade
 
 func _ready() -> void:
-	$title_Label.text = title
+	var char_number = title.length() + 2
+	var text = "#" + "#".repeat(char_number) + "#" + "\n" 
+	text += "#" + " ".repeat(char_number) + "#" + "\n"
+	text += "# " + title + " #\n"
+	text += "#" + " ".repeat(char_number) + "#" + "\n"
+	text += "#" + "#".repeat(char_number) + "#" + "\n"
+	
+	$title_Label.text = text
 	#$upgradeList_Node2D/bidon_upgrade_Node2D.blbl()
 
 #func _process(delta: float) -> void:
--- scripts/upgrade.gd
+++ scripts/upgrade.gd
@@ -5,18 +5,29 @@ extends Node2D
 @export var effect_acidity: float = -10
 @export var effect_energy: int = +10
 @export var price: int = 99
+@export var selected: bool = false
 
 func _ready() -> void:
 	$name_Label.text = name_str
 	if(effect_acidity != 0):
-		$effect_Label.text = "effect : acidity %f %/Day" % effect_acidity
+		$effect_Label.text = "effect : acidity %f %%/Day" % effect_acidity
 	if(effect_energy != 0):
 		$effect_Label.text = "effect : energy +%d W/Day" % effect_energy
 	$price_Label.text = "price : %d W" % price
-	#blbl()
+	
+	if selected:
+		select()
+	else:
+		deselect()
 
-#func _process(delta: float) -> void:
-	#pass
-
-#func blbl() -> void:
-	#print("BLBL effect : acidity %d m/day" % effect_acidity)
+func select() -> void:
+	selected = true
+	$pointer_Label.show()
+	$buy_Label.show()
+	$pointer_Label/BlinkLabel.process_mode = Node.PROCESS_MODE_INHERIT
+	
+func deselect() -> void:
+	selected = false
+	$pointer_Label.hide()
+	$buy_Label.hide()
+	$pointer_Label/BlinkLabel.process_mode = Node.PROCESS_MODE_DISABLED
